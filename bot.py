d223e90b6f6d3eaa7f2efcdba869084f892ca271
mike <samkomisha@gmail.com>
2021-07-02 15:00:51 +0300

    add logic for writing goals

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from config import TOKEN
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+import menu_buttons as mb
 
 
 logging.basicConfig(level=logging.INFO)
@@ -14,17 +15,40 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
+    await bot.send_message(message.from_user.id, f"Привет, {message.from_user.username}!",
+                           reply_markup=mb.main_menu)
 
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+    await message.reply("Введите /start для отображения меню")
 
 
 @dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text)
+async def echo_message(message: types.Message):
+    if message.text == '⚽ Записать голы':
+        await bot.send_message(message.from_user.id, '⚽ Записываю',
+                               reply_markup=mb.sub_menu_goals)
+    elif message.text == '1 Goal: ⚽':
+        await bot.send_message(message.from_user.id, "1x⚽", reply_markup=mb.sub_menu_goals_writing)
+
+    elif message.text == '2 Goals: ⚽⚽':
+        await bot.send_message(message.from_user.id, "2x⚽", reply_markup=mb.sub_menu_goals_writing)
+
+    elif message.text == '3 Goals: ⚽⚽⚽':
+        await bot.send_message(message.from_user.id, "3x⚽", reply_markup=mb.sub_menu_goals_writing)
+
+    elif message.text == '4 Goals: ⚽⚽⚽⚽':
+        await bot.send_message(message.from_user.id, "4x⚽", reply_markup=mb.sub_menu_goals_writing)
+
+    elif message.text == '5 Goals: ⚽⚽⚽⚽⚽':
+        await bot.send_message(message.from_user.id, "5x⚽", reply_markup=mb.sub_menu_goals_writing)
+
+    elif message.text == 'Завершить матч':
+        await bot.send_message(message.from_user.id, "Матч завершен", reply_markup=mb.main_menu)
+
+    elif message.text == 'Записать еще голы':
+        await bot.send_message(message.from_user.id, "Записываю еще голы", reply_markup=mb.sub_menu_goals)
 
 
 if __name__ == '__main__':
