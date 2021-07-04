0581a68afbf19d36e33be41a7d7d79370416c416
mike <samkomisha@gmail.com>
2021-07-04 14:32:17 +0300

    add writing goals to db function

--- app/helper_db_funcs.py
+++ app/helper_db_funcs.py
@@ -1,5 +1,5 @@
 from database.db_manager import dbman
-from queries_templates import ADDING_PLAYER_Q, SELECT_TG_ID_FROM_PERSONS
+from queries_templates import ADDING_PLAYER_Q, SELECT_TG_ID_FROM_PERSONS, ADD_GOALS
 
 
 def read_tg_id_from_person(conn):
@@ -14,3 +14,11 @@ def write_tg_id_to_db(conn, user_id, user_name):
                                               tg_name=f"'{user_name}'")
     dbman.execute_query(conn, add_player_query)
     conn.commit()  # commit changes
+
+
+def write_goals_to_db(conn, goals_num, tg_id, tg_name):
+    add_goals_query = ADD_GOALS.format(cur_tg_id=tg_id,
+                                       goals_num=goals_num,
+                                       cur_tg_name=f"'{tg_name}'")
+    dbman.execute_query(conn, add_goals_query)
+    conn.commit()
