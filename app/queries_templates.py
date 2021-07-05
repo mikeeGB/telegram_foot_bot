3dba4835c2ee65e96cc0cec5716c4c95f5c47405
mike <samkomisha@gmail.com>
2021-07-05 12:40:19 +0300

    add writing and updating goals/assists to team_stats table

--- app/queries_templates.py
+++ app/queries_templates.py
@@ -3,19 +3,32 @@ SELECT_TG_ID_FROM_PERSONS = "SELECT tg_id FROM person"
 ADD_GOALS = """INSERT INTO
                           stats(goals, assists, games_played, tg_id, tg_name)
                         VALUES({goals_num}, 0, 0, (SELECT tg_id FROM person WHERE tg_id = {cur_tg_id}),
-                               (SELECT tg_name FROM person WHERE tg_name = {cur_tg_name}))"""
+                               (SELECT tg_name FROM person WHERE tg_name = {cur_tg_name}));
+                INSERT INTO
+                          team_stats(games_played, goals, assists, winnings, defeats, draws)
+                        VALUES(0, {goals_num}, 0, 0, 0, 0);"""
 
 UPDATE_GOALS = """UPDATE stats
-                  SET goals = goals + {goals_num}
-                  WHERE tg_id = {cur_tg_id} AND cur_date = CURRENT_DATE"""
+                    SET goals = goals + {goals_num}
+                    WHERE tg_id = {cur_tg_id} AND cur_date = CURRENT_DATE;
+                  UPDATE team_stats
+                    SET goals = goals + {goals_num}
+                    WHERE cur_date = CURRENT_DATE;
+                  """
 
 SELECT_DATE_FROM_STATS = "SELECT cur_date FROM stats WHERE tg_id = {cur_tg_id}"
 
 ADD_ASSISTS = """INSERT INTO
                           stats(goals, assists, games_played, tg_id, tg_name)
                         VALUES(0, {assists_num}, 0, (SELECT tg_id FROM person WHERE tg_id = {cur_tg_id}),
-                               (SELECT tg_name FROM person WHERE tg_name = {cur_tg_name}))"""
+                               (SELECT tg_name FROM person WHERE tg_name = {cur_tg_name}));
+                INSERT INTO
+                          team_stats(games_played, goals, assists, winnings, defeats, draws)
+                        VALUES(0, 0, {assists_num}, 0, 0, 0);"""
 
 UPDATE_ASSISTS = """UPDATE stats
-                  SET assists = assists + {assists_num}
-                  WHERE tg_id = {cur_tg_id} AND cur_date = CURRENT_DATE"""
+                        SET assists = assists + {assists_num}
+                        WHERE tg_id = {cur_tg_id} AND cur_date = CURRENT_DATE;
+                    UPDATE team_stats
+                        SET assists = assists + {assists_num}
+                        WHERE cur_date = CURRENT_DATE;"""
