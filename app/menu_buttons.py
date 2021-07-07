55e5dfab9a54e63efae44a25f32d2d7ba91121e6
mike <samkomisha@gmail.com>
2021-07-07 12:25:38 +0300

    add return to main menu button

--- app/menu_buttons.py
+++ app/menu_buttons.py
@@ -16,10 +16,11 @@ btn_2_goal = KeyboardButton('2 Goals: ⚽⚽')
 btn_3_goal = KeyboardButton('3 Goals: ⚽⚽⚽')
 btn_4_goal = KeyboardButton('4 Goals: ⚽⚽⚽⚽')
 btn_5_goal = KeyboardButton('5 Goals: ⚽⚽⚽⚽⚽')
+btn_6_goal_menu = KeyboardButton('↩️Вернуться в меню')
 
 sub_menu_goals = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_1_goal, btn_2_goal,
                                                                btn_3_goal, btn_4_goal,
-                                                               btn_5_goal)
+                                                               btn_5_goal, btn_6_goal_menu)
 
 # sub menu asking goals
 btn_finish_writing_g = KeyboardButton('Завершить матч')
@@ -37,10 +38,12 @@ btn_2_assist = KeyboardButton('2 Assists: 🎯🎯')
 btn_3_assist = KeyboardButton('3 Assists: 🎯🎯🎯')
 btn_4_assist = KeyboardButton('4 Assists: 🎯🎯🎯🎯')
 btn_5_assist = KeyboardButton('5 Assists: 🎯🎯🎯🎯🎯')
+btn_6_assist_menu = KeyboardButton('↩️Вернуться в меню')
+
 
 sub_menu_assists = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_1_assist, btn_2_assist,
                                                                  btn_3_assist, btn_4_assist,
-                                                                 btn_5_assist)
+                                                                 btn_5_assist, btn_6_assist_menu)
 
 # sub menu asking assists
 btn_finish_writing_assists = KeyboardButton('Завершить матч')
@@ -50,3 +53,13 @@ btn_continue_writing_goals_2 = KeyboardButton('Записать голы')
 sub_menu_assists_writing = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_continue_writing_assists_2,
                                                                          btn_continue_writing_goals_2,
                                                                          btn_finish_writing_assists)
+
+
+# sub menu asking winning/defeat/draw
+btn_winning = KeyboardButton('👊 Победа')
+btn_defeat = KeyboardButton('🤬 Поражение')
+btn_draw = KeyboardButton('🤝 Ничья')
+
+sub_menu_winning_defeat_draw = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_winning,
+                                                                             btn_defeat,
+                                                                             btn_draw)
