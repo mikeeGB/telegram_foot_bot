ed25445fc6f1c82ee98ffd1e0fcb45a9760fc38f
mike <samkomisha@gmail.com>
2021-07-13 19:44:03 +0300

    add showing all time top players logic

--- app/helper_db_funcs.py
+++ app/helper_db_funcs.py
@@ -17,7 +17,9 @@ from queries_templates import ADDING_PLAYER_Q, SELECT_TG_ID_FROM_PERSONS, ADD_GO
                               SELECT_GAMES_PLAYED_FROM_TEAM_STATS_TODAY,\
                               SELECT_TOP_GOALSCORERS_TODAY,\
                               SELECT_TOP_ASSISTANTS_TODAY,\
-                              SELECT_TOP_GOAL_PLUS_ASSIST_TODAY
+                              SELECT_TOP_GOAL_PLUS_ASSIST_TODAY,\
+                              SELECT_TOP_GOALSCORERS_ALL_TIME,\
+                              SELECT_TOP_ASSISTANTS_ALL_TIME
 
 
 def read_tg_id_from_person(conn):
@@ -246,7 +248,7 @@ def show_top_goalscorers_today(conn):
     if type(data) is not str:
         message_text = f"Дата: {datetime.date.today()}\n"
         for i, data_tuple in enumerate(data):
-            message_text += f"{i+1}) {data_tuple[0]} - {data_tuple[1]}x⚽"
+            message_text += f"{i+1}) {data_tuple[0]} - {data_tuple[1]}x⚽\n"
     else:
         message_text = data
     return message_text
@@ -265,7 +267,7 @@ def show_top_assistants_today(conn):
     if type(data) is not str:
         message_text = f"Дата: {datetime.date.today()}\n"
         for i, data_tuple in enumerate(data):
-            message_text += f"{i+1}) {data_tuple[0]} - {data_tuple[1]}x🎯"
+            message_text += f"{i+1}) {data_tuple[0]} - {data_tuple[1]}x🎯\n"
     else:
         message_text = data
     return message_text
@@ -284,7 +286,45 @@ def show_top_goal_plus_assist_today(conn):
     if type(data) is not str:
         message_text = f"Дата: {datetime.date.today()}\n"
         for i, data_tuple in enumerate(data):
-            message_text += f"{i+1}) {data_tuple[0]} - {data_tuple[1]} points"
+            message_text += f"{i+1}) {data_tuple[0]} - {data_tuple[1]} points\n"
+    else:
+        message_text = data
+    return message_text
+
+
+def select_top_goalscorers_all_time(conn):
+    select_top_goalscorers_all_time_query = SELECT_TOP_GOALSCORERS_ALL_TIME
+    top_all_time_goalscorers_data = dbman.execute_read_query(conn, select_top_goalscorers_all_time_query)
+    if top_all_time_goalscorers_data:
+        return top_all_time_goalscorers_data
+    return "Нет статистики за сегодня"
+
+
+def show_top_goalscorers_all_time(conn):
+    data = select_top_goalscorers_all_time(conn)
+    if type(data) is not str:
+        message_text = ""
+        for i, data_tuple in enumerate(data):
+            message_text += f"{i+1}) {data_tuple[0]} - {data_tuple[1]}x⚽\n"
+    else:
+        message_text = data
+    return message_text
+
+
+def select_top_assistants_all_time(conn):
+    select_top_assistants_all_time_query = SELECT_TOP_ASSISTANTS_ALL_TIME
+    top_all_time_assistants_data = dbman.execute_read_query(conn, select_top_assistants_all_time_query)
+    if top_all_time_assistants_data:
+        return top_all_time_assistants_data
+    return "Нет статистики за сегодня"
+
+
+def show_top_assistants_all_time(conn):
+    data = select_top_assistants_all_time(conn)
+    if type(data) is not str:
+        message_text = ""
+        for i, data_tuple in enumerate(data):
+            message_text += f"{i+1}) {data_tuple[0]} - {data_tuple[1]}x🎯\n"
     else:
         message_text = data
     return message_text
